103307a5a79e6f35b87ebccd823c3cd934dd6a2b
Milinda Pathirage <milinda.pathirage@gmail.com>
2012-06-06 12:27:46 -0400

    Some improvements to adder based on rest of the blog post.

--- clojure-web/adder/src/adder/core.clj
+++ clojure-web/adder/src/adder/core.clj
@@ -3,7 +3,13 @@
   (:use hiccup.core)
   (:use hiccup.page)
   (:use ring.adapter.jetty)
-  (:use ring.middleware.params))
+  (:use ring.middleware.params)
+  (:use ring.middleware.reload)
+  (:use ring.middleware.stacktrace)
+  (:use ring.util.response)
+  (:use adder.middleware)
+  (:use ring.middleware.file)
+  (:use ring.middleware.file-info))
 
 (defn view-layout [& content]
   (html
@@ -12,15 +18,18 @@
                [:head
                 [:meta {:http-equiv "Content-type"
                         :content "text/html; charset=utf-8"}]
-                [:title "adder"]]
+                [:title "adder"]
+                [:link {:href "/css/adder.css" :rel "stylesheet" :type "text/css"}]]
                [:body content])))
 
-(defn view-input []
+(defn view-input [& [a b]]
   (view-layout
     [:h2 "add two numbers"]
     [:form {:method "post" :action "/"}
-     [:input.math {:type "text" :name "a"}] [:span.math "+"]
-     [:input.math {:type "text" :name "b"}] [:br]
+     (if (and a b)
+       [:p "those are not both numbers!"])
+     [:input.math {:type "text" :name "a" :value a}] [:span.math "+"]
+     [:input.math {:type "text" :name "b" :value b}] [:br]
      [:input.action {:type "submit" :value "add"}]]))
 
 (defn view-output [a b sum]
@@ -32,14 +41,27 @@
 (defn parse-input [a b]
   [(Integer/parseInt a) (Integer/parseInt b)])
 
-(defroutes myroutes
+(defroutes handler
            (GET "/" []
                 (view-input))
            (POST "/" [a b]
+                 (try
                    (let [[x y] (parse-input a b)
                          sum (+ x y)]
-                     (view-output x y sum))))
-(def app (wrap-params myroutes))
+                     (view-output x y sum))
+                 (catch NumberFormatException e
+                   (view-input a b))))
+           (ANY "/*" [path]
+                (redirect "/")))
+
+(def app (-> #'handler
+           (wrap-file "resources/public")
+           (wrap-file-info)
+           (wrap-request-logging)
+           (wrap-reload '[adder.core])
+           (wrap-bounce-favicon)
+           (wrap-stacktrace)
+           (wrap-params)))
 
 (defn -main []
   (run-jetty app {:port 8080}))
